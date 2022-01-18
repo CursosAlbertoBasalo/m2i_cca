2d1ee8e4fb5fed5ef8a1a34e0e18e81ce7926fdb
Alberto Basalo <albertobasalo@hotmail.com>
2022-01-18 10:24:05 +0100

    chore: remove classroom live coding

--- src/tmp-class-room/http.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable @typescript-eslint/no-unused-vars */
-/* eslint-disable @typescript-eslint/no-explicit-any */
-
-export class HTTP {
-  static request(url: string, options: any): any {
-    return { status: 200, body: { data: {} } };
-  }
-}
